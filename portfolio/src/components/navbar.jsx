1d73c37b6c29df7edda1d2a2341fc26c8509248b
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-12-18 11:12:56 +0300

    Complete

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import{AiOutlineMenuFold}from'react-icons/ai'
 
 export default function Navbar() {
     return (
-      <div className=" container items-center justify-center mx-auto text-gray-100  text-s">
+      <div className=" container items-center justify-center mx-auto text-gray-100 text-s">
           <nav className='py-10 flex justify-between'>
             <h1 className='text-s font-semibold font-mono mr-20 pl-20'>
                 <Link to='/'>
                 Home
                 </Link>
-                </h1>                              
+                </h1>   
+                <div className="hidden sm:block">
+                                    
               <ul className='flex items-center mr-20'>
               <li className='mr-4'>
                   <Link to='/proficiency'>
@@ -27,6 +30,9 @@ export default function Navbar() {
                 </Link>
                 </li>
               </ul>
+              </div>
+              <div/>
+              
           </nav>
           </div>
     )}
