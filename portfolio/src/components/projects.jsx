250e98dedc9f40937a7eeff9faf94cb8fcbe3317
Genevive-Mbesi <genmbesi@gmail.com>
2024-01-07 07:14:58 +0300

    Projects

--- portfolio/src/components/projects.jsx
+++ portfolio/src/components/projects.jsx
@@ -5,17 +5,16 @@ import {Card, CardHeader,CardFooter,Typography,CardBody,} from "@material-tailwi
 
 export default function Projects() {
     return ( 
-<Card className="mr-10 container ring-2 column-row font-mono text-gray-100 p-10 px-20 column-row mx-auto  ring-slate-500">
-      <CardHeader>
-        <img
-          src={gburudishe} alt ='g-burudishe'className=' shadow-indigo-500 p-3'
-        />
-      </CardHeader>
+<Card className=" font-mono text-gray-100  mx-20">
       <CardBody>
+        <img
+          src={gburudishe} alt ='g-burudishe'className='w-80  shadow-indigo-500 p-3'
+        />
         <Typography className="text-s">
 
 <span className="font-semibold"> g ~ burudishe </span> <br></br> 
- A cutting-edge web application revolutionizing search recommendations for relaxation chill spots by considering likes and personal preferences, the app tailors its suggestions aligning perfectly with your interests.
+ A cutting-edge web application revolutionizing,<br></br> search recommendations for relaxation <br></br> chill spots by considering likes and personal<br></br>
+  preferences, the app tailors its suggestions <br></br>aligning perfectly with your interests.
 <span className="font-semibold"><br></br>Key features include </span><br></br>
 
 Personalized recommendations and tailored suggestions<br></br> based on likes and preferences,<br></br>
