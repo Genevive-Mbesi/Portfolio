33817a195fc0018163950e4e22aee04b2d48b766
Genevive-Mbesi <genmbesi@gmail.com>
2024-02-05 12:49:37 +0300

    nav response

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -1,34 +1,25 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-      <div className=" container items-center justify-center mx-auto text-gray-100 text-s">
-          <nav className='py-10 flex justify-between'>
-            <h1 className='text-s font-semibold font-mono mr-20 pl-20'>
-                <Link to='/'>
-                Home
-                </Link>
-                </h1>
-              <ul className='flex items-center mr-20'>
-              <li className='mr-4'>
-                  <Link to='/proficiency'>
-                 Proficiency
-                </Link>
-                </li>
-                <li className='mr-4'>
-                  <Link to='/projects'>
-                 Projects
-                </Link>
-                </li>
-                <li >
-                  <Link to='/associations'>
-                 Tags
-                </Link>
-                </li>
-              </ul>
-              
-              <div/>
-          </nav>
-          </div>
-    )}
+  return (
+    <div className="container items-center justify-center mx-auto text-gray-100 text-s">
+      <nav className="py-10 flex flex-col items-center lg:flex-row lg:justify-between">
+        <h1 className="text-s font-semibold font-mono mb-4 lg:mb-0">
+          <Link to="/">Home</Link>
+        </h1>
+        <ul className="flex flex-col lg:flex-row lg:items-center">
+          <li className="mb-4 lg:mb-0 lg:mr-4">
+            <Link to="/proficiency">Proficiency</Link>
+          </li>
+          <li className="mb-4 lg:mb-0 lg:mr-4">
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/associations">Tags</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
