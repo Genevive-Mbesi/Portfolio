e6005a1c0eb9409eb47397d7de7ce7c5042cb324
Genevive-Mbesi <genmbesi@gmail.com>
2024-02-05 17:04:53 +0300

    layout adjustment

--- portfolio/src/components/home.jsx
+++ portfolio/src/components/home.jsx
@@ -15,8 +15,8 @@ export default function Home() {
         <h1 className='font-bold font-mono md:text-5xl'>Genevive<br></br>Mbesi</h1>
           <div className='text-s'> 
           <img src={assets} alt ='girl'className='w-40 rounded-full overflow-hidden container' />
-          <div className=' py-10 text-xl mx-auto font-mono'> <span className='text-gray-500'>Mbesi</span> is a software engineer and an enthusiastic creative artist,<br></br>she enjoys building projects by coming up with fun solutions <br></br>to solve modern world problems through tech.<br></br>
-           Her love for creativity has fostered her growth and journey in the tech space as she is always looking for ways to steer her creativity.
+          <div className=' py-10 text-xl mx-auto font-mono'> <span className='text-gray-500'>Mbesi</span> is a software engineer and an enthusiastic creative artist,<br></br>she enjoys building tech projects by coming up with fun solutions <br></br>to solve modern world problems.<br></br>
+           Her love for creativity has fostered her growth and journey in the tech space and she is always eager to apply her creativity.
           <Contacts/>
           <div className='py-2 font-mono text-xs'>
           click to view
