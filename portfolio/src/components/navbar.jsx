d194a66db78190f534e9a170a24d69e6e386c697
Genevive-Mbesi <genmbesi@gmail.com>
2024-02-05 12:56:46 +0300

    nav response

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="container items-center justify-center mx-auto text-gray-100 text-s">
-      <nav className="py-10 flex flex-col items-center lg:flex-row lg:justify-between">
+      <nav className="py-10 flex flex-col lg:flex-row lg:justify-between">
         <h1 className="text-s font-semibold font-mono mb-4 lg:mb-0">
           <Link to="/">Home</Link>
         </h1>
-        <ul className="flex flex-col lg:flex-row lg:items-center">
+        <div className="lg:hidden">
+          {/* Button to toggle the menu on small screens */}
+          <button onClick={toggleMenu}>Toggle Menu</button>
+        </div>
+        <ul
+          className={`${
+            showMenu ? "flex flex-col" : "hidden"
+          } lg:flex flex-col lg:flex-row lg:items-center`}
+        >
           <li className="mb-4 lg:mb-0 lg:mr-4">
             <Link to="/proficiency">Proficiency</Link>
           </li>
