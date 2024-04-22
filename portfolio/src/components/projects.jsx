bd72163f8b33bf9cbccbe7130a2dcb38875d87d3
mbesi <genmbesi@gmail.com>
2024-04-22 22:06:36 +0300

    ui/ux

--- portfolio/src/components/projects.jsx
+++ portfolio/src/components/projects.jsx
@@ -3,6 +3,7 @@ import './base.css';
 import { AiFillGithub } from 'react-icons/ai';
 import art from '../assets/artheart.png'
 import rental from '../assets/rtm.png'
+import portfolio from'../assets/Portfolio.jpeg'
 
 function Projects() {
   return (
@@ -31,6 +32,28 @@ function Projects() {
                 </div> 
               </div>
             </div>  
+            </div><div className="card-container">
+            <div className="card w-100 bg-rose-900 shadow-xl">
+              <figure><img src={portfolio} alt="Portfolio" /></figure>
+              <div className="card-body">
+                <h2 className="card-title text-neutral-100">
+                  Portfolio
+                  <div className='text-4xl'> 
+                  <a href= 'https://github.com/Genevive-Mbesi/Portfolio'>
+                     <AiFillGithub/>
+                     </a>
+                    </div>
+                </h2>
+                <p className='text-amber-700'>Website Portfolio</p>
+                <p className='text-neutral-100'>This website is  developed using <span className='text-amber-700'>React </span>
+                and styled with  <span className='text-amber-700'>Tailwind CSS </span>to ensure a seamless and visually appealing experience for customers.</p>
+                <div className="card-actions justify-end">
+                    <a href= 'https://mbesi.vercel.app'> 
+                    <div className="badge badge-outline text-neutral-100">View</div>
+                    </a>
+                </div> 
+              </div>
+            </div>  
             </div>
             <div className="card-container">
             <div className="card w-100 bg-rose-900 shadow-xl">
@@ -48,7 +71,7 @@ function Projects() {
                 <p className='text-neutral-100'>Rental Management System is a platform designed to help property owners
                             manage their rental properties efficiently,they can easily keep track of properties,tenants,and 
                             rental income,all in one place.
-                            Having developed the frontend, the following technologies were used <span className='text-amber-700'>JavaScript
+                            Having developed the frontend, the following technologies were used <span className='text-amber-700'>JavaScript,
                             React (React Router, React Bootstrap, React Auth Kit)</span>
                         </p>
                 <div className="card-actions justify-end"> 
